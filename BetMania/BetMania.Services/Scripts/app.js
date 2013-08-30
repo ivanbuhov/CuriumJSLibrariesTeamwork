9f0d949041faba775ea6112b3f3a287f7f6bb932
Bobbyborisov <borislav.borisov.tues@gmail.com>
2013-08-30 18:06:15 +0300

    Added app config file

---
+++ BetMania/BetMania.Services/Scripts/app.js
@@ -0,0 +1,41 @@
+﻿var app = app || {};
+
+
+app.config = (function () {
+    var layout = new kendo.Layout('main-layout');
+
+    var router = new kendo.Router({
+        init: function () {
+            layout.render('#application');
+        }
+    });
+
+    //mymatches default route
+    router.route('/', function () {
+        layout.showIn('#content', '');
+    });
+
+    router.route('/home', function () {
+        layout.showIn('#content', '');
+    });
+
+    //User routes
+    router.route('/user/login', function () {
+        layout.showIn('#content', '');
+    });
+
+    router.route('/user/logout', function () {
+        layout.showIn('#content', '');
+    });
+
+    //Matches routes
+    router.route('/allmatches', function () {
+        layout.showIn('#content','');
+    });
+
+    
+
+    $(function () {
+        router.start();
+    });
+}());
