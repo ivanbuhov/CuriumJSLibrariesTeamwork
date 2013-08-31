5249825101f89f1a5be5c8a22752485b46389c21
gparlakov <gparlakov@gmail.com>
2013-08-31 03:49:40 -0700

    add app-admin, viewModelsAdmin, viewsAdmin

---
+++ BetMania/BetMania.Services/Scripts/app-admin/viewsAdmin.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="../libs/_references.js" />
+betMania = betMania || {};
+
+betMania.views.admin = (function () {
+    var getMainAdminView = function () {
+        return new RSVP.Promise(function (resolve, reject) {
+            $.get("partials/admin.html")
+                .done(function (result) {
+                    resolve(result);
+                })
+                .fail(function (err) {
+                    reject(err)
+                });
+        })
+    };
+
+
+    return {
+        getMain: getMainAdminView
+    }
+}());
